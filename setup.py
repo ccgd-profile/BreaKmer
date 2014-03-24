7251aa04d3c0e4ed14d5abdbe576aa503bafe172
Ryan_Abo <rpa4@rchpc-prime.(none)>
2014-03-24 14:14:55 -0400

    Fixed error in setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(name='BreaKmer',
       version='0.1',
-      description=''
-      url='https://github.com/a-bioinformatician/BreaKmer'
-      author='Ryan Abo'
-      author_email='ryan.abo@gmail.com'
-      license='MIT'
+      description='',
+      url='https://github.com/a-bioinformatician/BreaKmer',
+      author='Ryan Abo',
+      author_email='ryan.abo@gmail.com',
+      license='MIT',
       py_modules=['BreaKmer'],
       install_requires=[
         'pysam',
