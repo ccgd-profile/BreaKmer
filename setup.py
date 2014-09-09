3822d8fe1ccf4fc44a67febf258cab7e2c39398f
Ryan_Abo <rpa4@rchpc-prime.(none)>
2014-09-09 11:54:39 -0400

    Changed dependency for biopython.

--- setup.py
+++ setup.py
@@ -2,16 +2,21 @@
 
 from setuptools import setup
 
+def readme() :
+  with open('README.md' as f:
+    return f.read()
+
 setup(name='BreaKmer',
-      version='0.1',
-      description='',
+      version='0.0.7',
+      description='Structural variation detection tool, designed for targeted sequencing data.',
+      long_description=readme(),
       url='https://github.com/a-bioinformatician/BreaKmer',
       author='Ryan Abo',
       author_email='ryan.abo@gmail.com',
-      license='MIT',
+      license='LICENSE',
       py_modules=['BreaKmer'],
       install_requires=[
-        'pysam',
-        'Bio'
+        'pysami >= 0.6',
+        'biopython >= 1.62'
       ]  
       )
