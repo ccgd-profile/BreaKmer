7c27c6ec9a7afd059aba6eb5a0870c20e0802a4d
ryanabo <ryanabo@ryans-imac.dfci.harvard.edu>
2015-06-12 13:38:30 -0400

    Bug fixes, refactoring.

--- breakmer/assembly/assembler.py
+++ breakmer/assembly/assembler.py
@@ -64,6 +64,7 @@ def init_assembly(kmers, fqRecs, kmerLen, rcThresh, readLen):
                 logger.info('Contig did not meet the read count threshold %d, with %d or contig length (%d) < readLen (%d)' % (rcThresh, len(contig.reads), len(contig.seq), readLen))
             else:
                 logger.info('Adding contig to buffer')
+                print 'breakmer.assembly.assembler init_assembly()', contig.seq
                 contigs.append(contig)
 
         # Clean up the data to free up memory.
--- breakmer/realignment/realigner.py
+++ breakmer/realignment/realigner.py
@@ -3,6 +3,7 @@
 
 import os
 
+
 __author__ = "Ryan Abo"
 __copyright__ = "Copyright 2015, Ryan Abo"
 __email__ = "ryanabo@gmail.com"
@@ -53,19 +54,60 @@ class Realignment:
         """
         """
         self.scope = scope
-        # alignment_program, extension, bin = align_params
+        # update
+        self.logger.info('Running blat %s, storing results in %s' % (self.params.opts['gfclient'], self.query_res_fn))
 
         self.result_fn = os.path.join(path, '%s_res.%s.%s' % (self.alignParams.program, scope, self.alignParams.extension))
         if self.alignParams.program == 'blast':
-            cmd = 
+            cmd = ''
         elif self.alignParams.program == 'blat':
-            cmd = 
+            # all blat server
+            cmd = '%s -t=dna -q=dna -out=psl -minScore=20 -nohead localhost %d %s %s %s' % (self.params.opts['gfclient'], self.params.opts['blat_port'], self.params.opts['reference_fasta_dir'], self.contig_fa_fn, self.query_res_fn)
+            # target
+            cmd = '%s -t=dna -q=dna -out=psl -minScore=20 -stepSize=10 -minMatch=2 -repeats=lower -noHead %s %s %s' % (self.params.opts['blat'], db, self.contig_fa_fn, self.query_res_fn)
+
+        # update
+        self.logger.info('Blat system command %s' % cmd)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        output, errors = p.communicate()
+        self.logger.info('Blat output %s' % output)
+        if errors != '':
+            self.logger.info('Blat errors %s' % errors)
 
         if not os.path.isfile(self.result_fn):
             return False
         else:
             self.result_manager = AlignResults(self.result_fn, self.alignParams.program, scope)
 
+"""
+    if self.contig_fa_fn :
+      self.run_blat(target_ref_fn, 'target') # Run blat against target reference sequence first for speed.
+      if not self.query_res_fn :
+        self.logger.info('No blat results file %s, no calls for %s.'%(self.query_res_fn, self.id))
+        return
+      if not self.check_target_blat(query_region) :
+        # Blat against whole genome reference fasta
+        self.run_blat(self.params.opts['reference_fasta'], 'all')
+  #*********************************************************
+
+  #*********************************************************
+  def run_blat(self, db, name) :
+    self.query_res_fn = os.path.join(self.path,'blat_res.'+name+'.psl')
+    if not os.path.isfile(self.query_res_fn) :
+      if name == 'all' : 
+        self.logger.info('Running blat %s, storing results in %s'%(self.params.opts['gfclient'],self.query_res_fn))
+        cmd = '%s -t=dna -q=dna -out=psl -minScore=20 -nohead localhost %d %s %s %s'%(self.params.opts['gfclient'],self.params.opts['blat_port'], self.params.opts['reference_fasta_dir'], self.contig_fa_fn, self.query_res_fn)
+      else :
+        self.logger.info('Running blat %s, storing results in %s'%(self.params.opts['blat'],self.query_res_fn))
+        cmd = '%s -t=dna -q=dna -out=psl -minScore=20 -stepSize=10 -minMatch=2 -repeats=lower -noHead %s %s %s'%(self.params.opts['blat'], db, self.contig_fa_fn, self.query_res_fn)
+
+      self.logger.info('Blat system command %s'%cmd)
+      p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+      output, errors = p.communicate()
+      self.logger.info('Blat output %s'%output)
+      if errors != '' : self.logger.info('Blat errors %s'%errors)
+    else : self.logger.info('Blat already run, results file %s exists, continuing'%self.query_res_fn)
+"""
 
 class AlignResults:
     def __init__(self, align_result_fn, program, scope):
