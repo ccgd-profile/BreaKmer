9965cb8b140747f5bcbfd55cee4066ae607d0bc7
ryanabo <ryanabo@ryans-imac.dfci.harvard.edu>
2015-06-23 16:23:04 -0400

    Bug fix

--- breakmer/processor/target.py
+++ breakmer/processor/target.py
@@ -505,7 +505,6 @@ class TargetManager:
         iter = 1
         contigs = self.variation.kmers['clusters']
         utils.log(self.loggingName, 'info', 'Resolving structural variants from %d kmer clusters' % len(contigs))
-        print 'breakmer.processor.target resolve_sv()', len(contigs), contigs
         for contig in contigs:
             contigId = self.name + '_contig' + str(iter)
             utils.log(self.loggingName, 'info', 'Assessing contig %s, %s' % (contigId, contig.seq))
