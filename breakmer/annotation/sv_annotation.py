29258dba50b984648b7ea2f3a2e16b5c7b83c38b
ryanabo <ryanabo@ryans-imac.dfci.harvard.edu>
2015-06-24 08:45:53 -0400

    Bug fix

---
+++ breakmer/annotation/sv_annotation.py
@@ -0,0 +1,9 @@
+#! /usr/bin/local/python
+# -*- coding: utf-8 -*-
+
+import breakmer.utils as utils
+
+__author__ = "Ryan Abo"
+__copyright__ = "Copyright 2015, Ryan Abo"
+__email__ = "ryanabo@gmail.com"
+__license__ = "MIT"
