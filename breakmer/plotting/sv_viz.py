0c948a4aec36e10827e0652d64810882c9d2ae5d
ryanabo <ryanabo@ryans-imac.dfci.harvard.edu>
2015-06-26 14:55:26 -0400

    Bug fix

--- breakmer/plotting/sv_viz.py
+++ breakmer/plotting/sv_viz.py
@@ -69,6 +69,7 @@ class Segment:
         trxItems = []
         trxIds = []
         for svBreakpoint in svBreakpoints:
+            print svBreakpoint
             print svBreakpoint.chrom, svBreakpoint.genomicCoords, svBreakpoint.targetKey
             annotatedTrxsDict = svBreakpoint.annotated_trxs
             dKeys = annotatedTrxsDict.keys()
