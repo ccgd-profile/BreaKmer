a1af4f4a012410825e0576baa8a73974308086e0
ryanabo <ryanabo@ryans-imac.dfci.harvard.edu>
2015-06-29 15:47:21 -0400

    Bug fix

--- breakmer/plotting/sv_viz.py
+++ breakmer/plotting/sv_viz.py
@@ -570,7 +570,7 @@ def plot_annotation_track(ax, yCoord, xOffset, segmentManager):
                 ll = [log(int(exon[0]), 2), log(int(exon[1]), 2)]
                 e1 = log(max(abs(int(genomicStart) - int(exon[0])), 1), 2) * bpUnits
                 e2 = log(max(abs(int(genomicStart) - int(exon[1])), 1), 2) * bpUnits
-                print 'Exon', exon[0], exon[1], trxOffset + e1
+                print 'Exon', exon[0], exon[1], trxOffset + e1, exon[0] - exon[1]
                 eCoords = [e1, e2]
                 eCoords.sort()
                 if reverse:
