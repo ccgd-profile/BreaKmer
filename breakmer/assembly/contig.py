de640a687270548ceae16c24d658cd494edf8763
ryanabo <ryanabo@ryans-imac.dfci.harvard.edu>
2015-07-01 14:13:10 -0400

    Bug fix

--- breakmer/assembly/contig.py
+++ breakmer/assembly/contig.py
@@ -515,9 +515,9 @@ class Builder:
             altKmers = get_read_kmers(read.seq, self.kmerLen, kmerTracker.kmerSeqs, '')
             print 'Alt kmers', altKmers
             print 'Contig kmers', contigKmers
-            print 'used mers', contigBuffer.used_mers
+            print 'used mers', contigBuffer.used_kmers
             print 'kmer set', kmerSet
-            newKmers = set(altKmers) - set(contigKmers) - contigBuffer.used_mers - kmerSet
+            newKmers = set(altKmers) - set(contigKmers) - contigBuffer.used_kmers - kmerSet
             if len(newKmers) > 0:
                 for kmerSeq in list(newKmers):
                     readCount = kmerTracker.get_count(kmerSeq)
