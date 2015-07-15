6ec1a7dfe3f76e65287f193c278daebfb233b98f
ryanabo <ryanabo@ryans-imac.dfci.harvard.edu>
2015-07-15 15:22:46 -0400

    Debugging prepare reference data.

--- breakmer/plotting/sv_viz.py
+++ breakmer/plotting/sv_viz.py
@@ -723,7 +723,7 @@ def plot_global_trx_track(ax, yCoord, xOffset, segmentManager):
         segTrxIter = 0
         for segTrx in segTrxs:
             # yCoord = yCoord + ((i + 0.5) * 0.25)
-            yCoord += 2
+            # yCoord += 2
             print 'Global trx ycoord', yCoord
             trxLen = float(segLen) / float(len(segTrxs))
             print 'TRX len', trxLen
