a6b1518420bb1a47f2ca6ee8cac2a434701b6c1c
Katie Abo <katieabo@Katies-iMac.local>
2015-08-18 16:49:24 -0400

    Bug fixes

--- breakmer/realignment/realigner.py
+++ breakmer/realignment/realigner.py
@@ -302,6 +302,7 @@ class AlignResults:
                 lResult = mergedResults[mapResults[mergedIdx[0]]]
                 newMergedIdx = mapResults[mergedIdx[0]]
             # Merge right and left values
+            print 'new merged idx', newMergedIdx
             mergedResults[newMergedIdx] = self.merge_record_fields(lResult, rResult)
             mapResults[mergeIdx[0]] = newMergedIdx
             mapResults[mergeIdx[1]] = newMergedIdx
