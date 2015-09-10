82abb8671c9dd1df4e9a7adfc04ffb2108f8699b
ryanabo <ryanabo@ryans-imac.dfci.harvard.edu>
2015-09-10 15:19:46 -0400

    Fixed a bug for reporting realign uniqueness.

--- breakmer/caller/filter.py
+++ breakmer/caller/filter.py
@@ -123,7 +123,7 @@ class ResultFilter:
         indelSizeThresh = int(self.params.get_param('indel_size'))
         utils.log(self.loggingName, 'info', 'Checking if blat result contains an indel variant')
         blatResult = svEvent.blatResults[0][1]
-        keep_br = blatResult.valid and blatResult.meanCov < 2 and blatResult.in_target and (blatResult.indel_maxevent_size[0] >= indelSizeThresh)
+        keep_br = blatResult.valid and blatResult.alignFreq < 2 and blatResult.in_target and (blatResult.indel_maxevent_size[0] >= indelSizeThresh)
         utils.log(self.loggingName, 'debug', 'Keep blat result %r' % keep_br)
 
         # Determine the uniqueness of the realignment.
