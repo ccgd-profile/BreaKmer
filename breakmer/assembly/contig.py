e8144e91b47ea04f32016a34ac3e02f9114ed456
ryanabo <ryanabo@ryans-imac.dfci.harvard.edu>
2015-11-25 11:30:29 -0500

    Added print statements for debugging.

--- breakmer/assembly/contig.py
+++ breakmer/assembly/contig.py
@@ -162,6 +162,9 @@ class ContigCounts:
         Return:
             counts: List of integers for counts of reads assembled at the provided range.
         """
+
+        print 'Contig stored counts, indel_only', self.indel_only
+        print 'Contig stored counts, others', self.others
         counts = []
         if sv_type == 'indel' or sv_type == 'rearr':
             if p1 == p2:
