dffdcfa0a22ede337e51ef1fabe879d726660f68
ryanabo <ryanabo@ryans-imac.dfci.harvard.edu>
2015-11-25 11:35:53 -0500

    Removed unecessary code in main script.

--- breakmer.py
+++ breakmer.py
@@ -1,7 +1,6 @@
 #! /usr/bin/local/python
 # -*- coding: utf-8 -*-
 
-import sys
 import argparse
 import breakmer.params as params
 import breakmer.processor.analysis as breakmer_analysis
@@ -20,7 +19,6 @@ There are three functions provided:
 2. start_blat_server      = start the blat server in the background for analysis.
 3. prepare_reference_data = prepare the reference data for the target regions that are specified in the input files.
 
-
 The blat server provides a challenge in workflow. The best method is to:
 1. prepare reference data using 'prepare_reference_data' function
 2. start the blat server using 'start_blat_server' function
@@ -30,15 +28,15 @@ The blat server provides a challenge in workflow. The best method is to:
 
 '''
 
-args = sys.argv
-
 PARSER = argparse.ArgumentParser(description='Program to identify structural variants within targeted locations.', usage='%(prog)s [options]', add_help=True)
 SUBPARSERS = PARSER.add_subparsers(help='Program mode (run, start_blat_server, prepare_reference_data).', dest='fncCmd')
 
+# Setup three separate parsers for the three different functions.
 RUN_PARSER = SUBPARSERS.add_parser('run', help='Run analysis to detect structural variants.')
 SERVER_PARSER = SUBPARSERS.add_parser('start_blat_server', help='Start the blat server prior to performing the analysis.')
 REF_PARSER = SUBPARSERS.add_parser('prepare_reference_data', help='Prepare the reference sequence data for target regions prior to analysis.')
 
+# Run parser
 RUN_PARSER.add_argument('--log_level', dest='log_level', default='DEBUG', help='Log level [default: DEBUG]')
 RUN_PARSER.add_argument('--indel_size', dest='indel_size', default=15, type=int, help='Indel size filter. [default: %(default)s]')
 RUN_PARSER.add_argument('--trl_sr_thresh', dest='trl_sr_thresh', default=2, type=int, help='Split read support threshold for translocations. [default: %(default)s]')
@@ -59,13 +57,16 @@ RUN_PARSER.add_argument('-k', '--keep_blat_server', dest='keep_blat_server', def
 RUN_PARSER.add_argument('-p', '--port_number', dest='blat_port', default=None, type=int, help='The port number for the blat server. A random port number (8000-9500) will be used if not specified. [default: %(default)s]')
 RUN_PARSER.add_argument('-c', '--config', dest='config_fn', default=None, required=True, help='The configuration filename that contains additional parameters. [default: %(default)s]')
 
+# Server parser
 SERVER_PARSER.add_argument('-p', '--port_number', dest='blat_port', default=None, type=int, help='The port number for the blat server. A random port number (8000-9500) will be used if not specified. [default: %(default)s]')
 SERVER_PARSER.add_argument('--hostname', dest='blat_hostname', default='localhost', help='The hostname for the blat server. Localhost will be used if not specified. [default: %(default)s]')
 SERVER_PARSER.add_argument('-c', '--config', dest='config_fn', default=None, required=True, help='The configuration filename that contains additional parameters. [default: %(default)s]')
 
+# Setup reference parser
 REF_PARSER.add_argument('-g', '--gene_list', dest='gene_list', default=None, help='Gene list to consider for analysis. [default: %(default)s]')
 REF_PARSER.add_argument('-c', '--config', dest='config_fn', default=None, required=True, help='The configuration filename that contains additional parameters. [default: %(default)s]')
 REF_PARSER.add_argument('-n', '--nprocessors', dest='nprocs', default=1, type=int, help='The number of processors to use for analysis. [default: %(default)s]')
 
+# Start analysis
 RUN_TRACKER = breakmer_analysis.RunTracker(params.ParamManager(PARSER.parse_args()))
 RUN_TRACKER.run()
