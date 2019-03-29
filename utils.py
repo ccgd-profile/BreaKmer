7b61d2ab3f848d439cc53dd838a5fcbc8b7208d3
Matt Ducar <md299@BagEnd.local>
2019-03-29 09:34:27 -0400

    Resolving issues with previous check ins

--- utils.py
+++ utils.py
@@ -96,7 +96,10 @@ def check_intervals(breakpts, query_region ) :
 
 #-----------------------------------------------------------
 def setup_logger(param_opts, name) :
-  output_path = os.path.abspath(os.path.normpath(param_opts['output']))
+  # This is a bit dangerous.  It relies on setting an "output" directory under the analysis directory
+  output_path = os.path.abspath(os.path.normpath(param_opts['analysis_dir']))
+  if not os.path.exists(output_path) : os.makedirs(output_path)
+
   if not os.path.exists(output_path) : os.makedirs(output_path)
 
   logger = logging.getLogger(name)
